56da21f817cf1780b6c061dabcf13e877fdda735
fadi <fadielsmaily@gmail.com>
2017-04-26 20:00:18 -0400

    part II

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Fazzi'}
+    posts = [
+        {
+            'author': {'nickname':'Zen Shaw'},
+            'body': 'Beautiful day for a Meat Module!'
+        },
+        {
+            'author': {'nickname':'Alberto'},
+            'body': 'Everything in the world is because Portugal exists'
+        }
+    ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
