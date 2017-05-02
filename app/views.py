22fb7937109310edb83b97dd44ea694276cdfb95
fadi <fadielsmaily@gmail.com>
2017-05-01 22:13:53 -0400

    part 3 + gitignore

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect, session
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
+    if not 'user' in session:
+        return redirect('/login')
+
     user = {'nickname': 'Fazzi'}
     posts = [
         {
@@ -20,3 +23,25 @@ def index():
                             title='Home',
                             user=user,
                             posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+
+        session['user'] = form.openid.data
+
+        return redirect('/index')
+
+    return render_template('login.html',
+                            title= 'Sign in',
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
+
+@app.route('/logout')
+def logout():
+    # session.pop('user')
+    session.clear()
+    return redirect('/')
---
+++ db_create.py
@@ -0,0 +1,13 @@
+#!/Users/fazzi/anaconda3.5.2/envs/microblog/bin/python
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database Repo')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
