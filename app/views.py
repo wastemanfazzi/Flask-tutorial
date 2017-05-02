5f2323fd74512f011c35afa7037100a8e2b5de28
fadi <fadielsmaily@gmail.com>
2017-05-02 18:02:49 -0400

    part 5 openid

--- app/views.py
+++ app/views.py
@@ -1,14 +1,22 @@
-from flask import render_template, flash, redirect, session
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
+
+#returns all user id(primary_keys) from sqlite table using ORM
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     if not 'user' in session:
         return redirect('/login')
 
-    user = {'nickname': 'Fazzi'}
+    user = g.user
     posts = [
         {
             'author': {'nickname':'Zen Shaw'},
@@ -25,23 +33,51 @@ def index():
                             posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-
-        session['user'] = form.openid.data
-
-        return redirect('/index')
-
+        session['remember_me'] = form.remember_me
+        return oid.try_login(form.openid.data, ask_for = ['nickname','email'])
+        #flash('Login requested for OpenID="%s", remember_me=%s' %
+              #(form.openid.data, str(form.remember_me.data)))
     return render_template('login.html',
                             title= 'Sign in',
                             form=form,
                             providers=app.config['OPENID_PROVIDERS'])
 
+        #session['user'] = form.openid.data
+
+        #return redirect('/index')
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email =="":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
 @app.route('/logout')
 def logout():
     # session.pop('user')
-    session.clear()
-    return redirect('/')
+    logout_user()
+    return redirect(url_for('index'))
